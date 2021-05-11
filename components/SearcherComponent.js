a2ce01fcea505ee22da07e1a0eed7400b67198fc
Kuba Nowacki <me@kubanowacki.eu>
2021-05-11 17:00:39 +0200

    11.05

--- components/SearcherComponent.js
+++ components/SearcherComponent.js
@@ -8,7 +8,6 @@ export default class SearcherComponent extends React.Component
         super(props);
 
         this.currentCity = NaN;
-        this.listRefresh = false;
         this.onChange = this.onChange.bind(this);
         this.citiesList = [];
     }
@@ -20,13 +19,19 @@ export default class SearcherComponent extends React.Component
 
     getByCurrentLocation() {
         this.props.getCitiesByLocation(this.props.location.coords.latitude, this.props.location.coords.longitude).then((res) => {
-            res.data.list.forEach(elem => {
-                //console.log(elem.name + " | " + elem.id + " | " + elem.coord.lat + " | " + elem.coord.lon);
-                this.citiesList.push({key: elem.name});
-            });
-            this.listRefresh = true;
-            console.log(this.citiesList);
+            this.citiesList = res.data.list;
+            this.forceUpdate();
         });
+    }
+
+    clickOnBoxContentValue(value) {
+        this.closeModal();
+        console.log(value);
+    }
+
+    closeModal() {
+        this.props.changeModalVisibility(false);
+        this.citiesList = [];
     }
 
     render() {
@@ -36,9 +41,7 @@ export default class SearcherComponent extends React.Component
                     animationType="slide"
                     transparent={true}
                     visible={this.props.modalVisible}
-                    onRequestClose={() => {
-                        this.props.changeModalVisibility(false);
-                    }}
+                    onRequestClose={() => { this.closeModal() }}
                 >
                     <View style={styles.modalBoxContainer}>
                         <View style={styles.modalBoxHeader}>
@@ -53,11 +56,15 @@ export default class SearcherComponent extends React.Component
                                     <TextInput
                                         onChangeText={this.currentCity}
                                         placeholder="Wpisz miejścowość"
+                                        style={styles.modalBoxInputTextInput}
                                     ></TextInput>
                                 </View>
 
                                 <View style={styles.modalBoxClose}>
-                                    <Pressable onPress={() => this.props.changeModalVisibility(false)}>
+                                    <Pressable onPress={() => {
+                                        this.closeModal();
+                                        this.citiesList = [];
+                                    }}>
                                         <Icon name='close' color="#fff"></Icon>
                                     </Pressable>
                                 </View>
@@ -67,8 +74,13 @@ export default class SearcherComponent extends React.Component
                         <View style={styles.modalBoxContent}>
                             <FlatList
                                 data={this.citiesList}
-                                renderItem={({item}) => <Text>{item.key}</Text>}
-                                extraData={this.listRefresh}
+                                renderItem={({item}) =>
+                                    <Pressable onPress={() => {
+                                        this.clickOnBoxContentValue(item.id)
+                                    }}>
+                                        <Text style={styles.modalBoxContentValue}>{item.name}</Text>
+                                    </Pressable>
+                                }
                             />
                         </View>
                     </View>
@@ -102,12 +114,25 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 3,
         borderColor: "#c2c2c2",
-        width: "74%"
+        width: "74%",
+    },
+    modalBoxInputTextInput: {
+        textAlign: "center"
     },
     modalBoxClose: {
         width: "13%"
     },
     modalBoxContent: {
         marginTop: 15
+    },
+    modalBoxContentValue: {
+        width: "95%",
+        fontSize: 20,
+        marginTop: "5%",
+        alignSelf: "center",
+        borderWidth: 1,
+        borderRadius: 3,
+        borderColor: "#e2e2e2",
+        textAlign: "center"
     }
 });
